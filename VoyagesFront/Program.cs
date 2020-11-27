ded5592706d03b12c4b01369f21b5421ae930d38
S.Labasse <labasse@gmail.com>
2020-11-27 16:38:49 +0100

    04_02 - Journaliser l'exécution

--- VoyagesFront/Program.cs
+++ VoyagesFront/Program.cs
@@ -18,6 +18,7 @@ namespace VoyagesFront
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
 
             await builder.Build().RunAsync();
         }
